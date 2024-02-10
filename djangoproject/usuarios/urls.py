5f04f82122a01ba038ee0e9bdbb7d515a95ca938
LeireJm <leirejim@ucm.es>
2024-02-10 12:24:20 +0100

    inicio de sesion empezado

---
+++ djangoproject/usuarios/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+#rutas -> poner carpeta y archivo import nombre de la funcion
+from . import views
+
+#path ('lo de aquí es la ruta en la url', ), es decir, si pongo perfil/ significa que en la web es noseque/perfil
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
